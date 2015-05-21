1bb1d3ef07b4df4e402fa5bccd4e7d7bc8803990
weathersboyle <rob@intrepid.io>
2015-05-21 17:05:03 -0400

    created fragment to show blog feed; adjusted resource attibutes

---
+++ app/src/main/java/com/robboyle/lawblog/BlogsFragment.java
@@ -0,0 +1,24 @@
+package com.robboyle.lawblog;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlogsFragment extends Fragment {
+
+    public BlogsFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_blogs, container, false);
+    }
+
+}
